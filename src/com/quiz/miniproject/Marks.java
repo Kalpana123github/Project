5fae918b748790ee0877ffa1ee08344697ce6625
kamini chavan <kamini.chavan14@gmail.com>
2023-01-24 22:24:40 +0530

    grade updated

---
+++ src/com/quiz/miniproject/Marks.java
@@ -0,0 +1,32 @@
+package com.quiz.miniproject;
+
+public class Marks {
+
+	
+		int marks=00;
+		public void DisplayResult() {
+			
+			if(marks>=8 && marks<=10) {
+				System.out.println("class A");
+			}
+			else if(marks>=6 && marks<=8) {
+				System.out.println("class B");
+			}
+			else if(marks==5) {
+				System.out.println("class C");
+			}
+			else if (marks<5){
+				System.out.println("Fails");
+			}
+			
+		}
+		public static void main(String[] args) {
+			
+			Marks D1 = new Marks();
+			D1.DisplayResult();
+			
+		}
+
+	}
+
+
